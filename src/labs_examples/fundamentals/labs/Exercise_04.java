6bc146736edfdb1617681a11f2f0aeaaa03747f2
Daniel Zhang <daniel6628@gmail.com>
2022-11-12 22:08:40 -0500

    finished

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -2,10 +2,9 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 4: My Second Program
- *
- *      Write the necessary code to display, declare and print each of Java's primitive data types.
- *      Please declare a single variable of each type, give it an appropriate value, then print that variable.
- *
+ * <p>
+ * Write the necessary code to display, declare and print each of Java's primitive data types.
+ * Please declare a single variable of each type, give it an appropriate value, then print that variable.
  */
 
 public class Exercise_04 {
@@ -14,9 +13,27 @@ public class Exercise_04 {
 
         int i = 1;
         System.out.println("int i is: " + i);
-
         // write your code below
+        char c = 'c';
+        System.out.println("char l is: " + c);
 
+        boolean t = true;
+        System.out.println("boolean t is: " + t);
+
+        long l = 500L;
+        System.out.println("long l is: " + l);
+
+        short s = 10;
+        System.out.println("short s is: " + s);
+
+        float f = 0.10f;
+        System.out.println("float f is: " + f);
+
+        double d = 210.90d;
+        System.out.println("double d is: " + d);
+
+        byte b = 10;
+        System.out.println("byte b is: " + b);
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,27 +1,33 @@
 package labs_examples.fundamentals.labs;
 
 
-/**
- * Fundamentals Exercise 5: Working with Strings
- *
- *      Please follow the instructions in the comments below
- *
- */
-
 public class Exercise_05 {
 
     public static void main(String[] args) {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int len = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean isEqual = str.equals(str2);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String newStr = str + str2;
+        System.out.println(newStr);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String replaceResult = str.replace('o', 'a');
+        System.out.println(replaceResult);
+
+        String subStringResult = str2.substring(0,3);
+        System.out.println(subStringResult);
+
+        boolean containResult = str2.contains("a");
+        System.out.println(containResult);
+
+        int indexResult = str.indexOf("o");
+        System.out.println(indexResult);
 
     }
 
