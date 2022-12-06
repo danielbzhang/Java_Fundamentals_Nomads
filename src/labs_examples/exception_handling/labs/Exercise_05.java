7ce75e415fad15c1339d62e34a8b4d8fb93b7d34
Daniel Zhang <daniel6628@gmail.com>
2022-12-06 16:21:36 -0500

    exception

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -12,7 +12,41 @@ import java.util.Arrays;
 class Example {
 
     public static void main(String[] args) {
-        
+        try {
+            methodOne();
+        } catch (ExceptionExample exc) {
+            exc.toString();
+        }
+
     }
 
+    public static void methodOne() throws ExceptionExample {
+        try{
+            methodTwo();
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println(exc.toString());
+        }
+
+    }
+
+    public static void methodTwo() throws ArrayIndexOutOfBoundsException {
+        int[] nums = {0,1,2,3};
+//        try{
+//            int x = nums[7];
+//        } catch (ArrayIndexOutOfBoundsException exc) {
+//            System.out.println("Exception caught in methodTwo");
+//            throw exc;
+//        }
+
+        int x = nums[8];
+        System.out.println(x);
+    }
+
+}
+
+class ExceptionExample extends Exception{
+    @Override
+    public String toString() {
+        return "Customized Exception!";
+    }
 }
