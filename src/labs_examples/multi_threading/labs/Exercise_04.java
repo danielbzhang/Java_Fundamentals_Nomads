e732f247bc3a0956e7d7199fb9f8d7990ff6f4cd
Daniel Zhang <daniel6628@gmail.com>
2022-12-13 11:20:21 -0500

    thread04

--- src/labs_examples/multi_threading/labs/Exercise_04.java
+++ src/labs_examples/multi_threading/labs/Exercise_04.java
@@ -2,7 +2,58 @@ package labs_examples.multi_threading.labs;
 
 /**
  * Multithreading Exercise 4:
- *
- *      Demonstrate the use of a synchronized block and a synchronized method - ensure that the synchronization is
- *      working as expected
+ * <p>
+ * Demonstrate the use of a synchronized block and a synchronized method - ensure that the synchronization is
+ * working as expected
  */
+class Sender {
+    public synchronized void send(String msg) {
+        System.out.println("Sending\t" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Thread interrupted.");
+        }
+        System.out.println("\n" + msg + "Sent.");
+    }
+}
+
+class ThreadedSend extends Thread {
+    private String msg;
+    private Thread t;
+    Sender sender;
+
+    ThreadedSend(String m, Sender obj) {
+        msg = m;
+        sender = obj;
+    }
+
+    //    public void run() {
+//        synchronized (sender) {
+//            sender.send(msg);
+//        }
+//    }
+    @Override
+    public void run() {
+        sender.send(msg);
+    }
+}
+
+class SyncDemo {
+    public static void main(String[] args) {
+        Sender snd = new Sender();
+        ThreadedSend s1 = new ThreadedSend("hi", snd);
+        ThreadedSend s2 = new ThreadedSend("bye", snd);
+
+        s1.start();
+        s2.start();
+
+        try {
+            s1.join();
+            //  main thread will wait until s1 completes before moving forward.
+            s2.join();
+        } catch (Exception e) {
+            System.out.println("Interrupted.");
+        }
+    }
+}
--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -2,7 +2,40 @@ package labs_examples.multi_threading.labs;
 
 /**
  * Multithreading Exercise 5:
- *
  *      Demonstrate the use of a wait() and notify()
  */
 
+class Data{
+    private String packet;
+    private boolean transfer = true;
+
+    public synchronized void send(String packet) {
+        while (!transfer) {
+            try{
+                wait();
+            } catch (InterruptedException exc) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread interrupted -- send");
+            }
+        }
+        transfer = false;
+        this.packet = packet;
+        notifyAll();
+    }
+
+    public synchronized String receive() {
+        while (transfer) {
+            try {
+                wait();
+            } catch (InterruptedException exc) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread interrupted -- receive");
+            }
+        }
+
+        transfer = true;
+
+        notifyAll();
+        return packet;
+    }
+}
