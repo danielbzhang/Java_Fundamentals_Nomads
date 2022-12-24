877de260c7634e840c5ac6183f91900ee6814a3e
Daniel Zhang <daniel6628@gmail.com>
2022-12-24 15:04:08 -0500

    lambda ex3

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,11 @@
 package labs_examples.lambdas.labs;
 
+import java.lang.invoke.MethodHandle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -8,3 +14,28 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+
+class MethodReference {
+    public static boolean isMoreThanFifty(int num1, int num2) {
+        return (num1 + num2) > 50;
+    }
+
+    public static List<Integer> findNumbers(List<Integer> lst, BiPredicate<Integer, Integer> p) {
+        List<Integer> newList = new ArrayList<>();
+        for(Integer i : lst) {
+            if(p.test(i, i+10)) {
+                newList.add(i);
+            }
+        }
+        return newList;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(12, 5, 45, 18, 33, 24, 40);
+
+        // Using a lambda expression
+        findNumbers(list, (i1, i2) -> isMoreThanFifty(i1, i2));
+        // Using a method reference
+        System.out.println(findNumbers(list, MethodReference :: isMoreThanFifty));
+    }
+}
