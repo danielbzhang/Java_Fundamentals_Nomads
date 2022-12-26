44398b7bf56d4b1f467518acc3ddeeea924feaad
Daniel Zhang <daniel6628@gmail.com>
2022-12-26 16:26:11 -0500

    streams

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import static labs_examples.lambdas.labs.MethodReference.findNumbers;
 
 /**
  * Lambdas Exercise 3:
- *
- *      1) Demonstrate the use of a static method reference
- *      2) Demonstrate the use of an instance method reference
- *      3) Demonstrate the use of a constructor reference
- *
+ * <p>
+ * 1) Demonstrate the use of a static method reference
+ * 2) Demonstrate the use of an instance method reference
+ * 3) Demonstrate the use of a constructor reference
  */
 
 class MethodReference {
@@ -24,8 +24,8 @@ class MethodReference {
 
     public static List<Integer> findNumbers(List<Integer> lst, BiPredicate<Integer, Integer> p) {
         List<Integer> newList = new ArrayList<>();
-        for(Integer i : lst) {
-            if(p.test(i, i+10)) {
+        for (Integer i : lst) {
+            if (p.test(i, i + 10)) {
                 newList.add(i);
             }
         }
@@ -33,13 +33,43 @@ class MethodReference {
     }
 }
 
-class MethodReferenceController{
+class MethodReferenceController {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(12, 5, 45, 18, 33, 24, 40);
 
         // Using a lambda expression
         findNumbers(list, (i1, i2) -> MethodReference.isMoreThanFifty(i1, i2));
         // Using a method reference
-        System.out.println(findNumbers(list, MethodReference :: isMoreThanFifty));
+        System.out.println(findNumbers(list, MethodReference::isMoreThanFifty));
+
+        List<Shipment> l = new ArrayList<Shipment>();
+        // calculateOnShipments(l, Shipment :: calculateWeight);
+    }
+
+}
+
+class Shipment {
+    public double calculateWeight() {
+        double weight = 0;
+        return weight;
+    }
+
+    public List<Double> calculateOnShipments(List<Shipment> l, Function<Shipment, Double> f) {
+        List<Double> results = new ArrayList<>();
+        for (Shipment s : l) {
+            results.add(f.apply(s));
+        }
+        return results;
+    }
+}
+
+class ArbitraryObjExample{
+    public static void main(String[] args) {
+        String[] stringArray = {"Nick", "Caden", "Bob"};
+
+        Arrays.sort(stringArray, String::compareToIgnoreCase);
+        for(String s : stringArray) {
+            System.out.println(s);
+        }
     }
 }
--- src/labs_examples/lambdas/labs/Exercise_04.java
+++ src/labs_examples/lambdas/labs/Exercise_04.java
@@ -3,31 +3,59 @@ package labs_examples.lambdas.labs;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * Lambdas Exercise 4:
- *
- *      Stream API Labs
- *
- *      1) Demonstrate the use of the range function to print out the numbers 1 through 15 (inclusive)
- *      2) Demonstrate the use of the sum function to determine the range of a set of numbers.
- *      3) Demonstrate the use of the map() function to alter each int in a List of Integers, then use the sum function
- *          to get the sum of the modified list
- *      4) Demonstrate the filter function by filtering out all Integers that are less than 10 - then use the average
- *          function to average the remaining numbers, assign this result to an int variable.
- *      5) Demonstrate the reduce() function to determine the sum of a list of Integers
- *      6) Demonstrate how to Stream a text file and print out each line
- *      7) Demonstrate how to Stream the stream_text_lab.csv file in this package. Split the lines into String arrays,
- *          then print out the element at the 1 index for each array.
- *      8) Demonstrate how to Stream the stream_text_lab.csv file in this package. Split the lines into String arrays,
- *          then print out the sum of all elements at index 2.
- *      9) Demonstrate the anyMatch() function.
- *      10) Demonstrate the allMatch() function.
- *      11) Demonstrate the collect() terminal operation to store resulting values into a List
- *      
+ * <p>
+ * Stream API Labs
+ * <p>
+ * 1) Demonstrate the use of the range function to print out the numbers 1 through 15 (inclusive)
+ * 2) Demonstrate the use of the sum function to determine the range of a set of numbers.
+ * 3) Demonstrate the use of the map() function to alter each int in a List of Integers, then use the sum function
+ * to get the sum of the modified list
+ * 4) Demonstrate the filter function by filtering out all Integers that are less than 10 - then use the average
+ * function to average the remaining numbers, assign this result to an int variable.
+ * 5) Demonstrate the reduce() function to determine the sum of a list of Integers
+ * 6) Demonstrate how to Stream a text file and print out each line
+ * 7) Demonstrate how to Stream the stream_text_lab.csv file in this package. Split the lines into String arrays,
+ * then print out the element at the 1 index for each array.
+ * 8) Demonstrate how to Stream the stream_text_lab.csv file in this package. Split the lines into String arrays,
+ * then print out the sum of all elements at index 2.
+ * 9) Demonstrate the anyMatch() function.
+ * 10) Demonstrate the allMatch() function.
+ * 11) Demonstrate the collect() terminal operation to store resulting values into a List
  */
 
 class Example {
-    
+    public static void main(String[] args) throws IOException {
+        // 1
+//        IntStream.range(1, 16).forEach(System.out :: print);
+
+        // 2
+        int sum = IntStream.range(1, 10).sum();
+//        System.out.println(sum);
+
+        // 3
+        int sum2 = IntStream.range(1, 10).map(x -> x * x).sum();
+//        System.out.println(sum2);
+
+        // 4
+//        Arrays.stream(new int[]{2, 3, 14, 56, 100}).filter(x -> x > 10).average().ifPresent(System.out::println);
+
+        // 5
+        int sum3 = IntStream.range(1, 20).reduce(0, (int a, int b) -> a + b);
+//        System.out.println(sum3);
+
+        // 6, 7, 8
+        String fileAddress = "src/labs_examples/lambdas/labs/stream_text_lab.csv";
+        Stream<String> file = Files.lines(Paths.get(fileAddress));
+//        file.forEach(System.out :: println);
+        file.close();
+
+        // 9
+        
+    }
 }
