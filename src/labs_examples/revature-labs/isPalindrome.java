a06f6257bff032716a73aa90c70b33d53c28ebfa
Daniel Zhang <daniel6628@gmail.com>
2023-01-28 10:10:44 -0500

    subarray

--- src/labs_examples/revature-labs/isPalindrome.java
+++ src/labs_examples/revature-labs/isPalindrome.java
@@ -1,25 +1,28 @@
 
-public class Palindrome {
+public class isPalindrome {
   /**
-   * Palindromes are words that are the same forwards as they are backwards - eg, 'bob', 'racecar'.
+   * Palindromes are words that are the same forwards as they are backwards - eg,
+   * 'bob', 'racecar'.
    *
    * You can solve this problem in several ways:
-   * You could start with a String reversal algorithm, which you have already written, and check if str is the same
-   * as its reversed form, or, you could check if the character at the nth position of a String moving forwards
+   * You could start with a String reversal algorithm, which you have already
+   * written, and check if str is the same
+   * as its reversed form, or, you could check if the character at the nth
+   * position of a String moving forwards
    * re the same as the nth position of a String moving backwards.
    *
    * @param str A String.
    * @return true if str is a palindrome, false otherwise.
    */
-  public boolean pal(String str){
-      String copy = "";
-      for (int i = 0; i < str.length(); i++) {
-          copy = str.charAt(i)+copy;
-      }
-      if(str.compareTo(copy) == 0) {
-          return true;
-      }
+  public boolean pal(String str) {
+    String copy = "";
+    for (int i = 0; i < str.length(); i++) {
+      copy = str.charAt(i) + copy;
+    }
+    if (str.compareTo(copy) == 0) {
+      return true;
+    }
 
-      return false;
+    return false;
   }
 }
---
+++ src/labs_examples/revature-labs/subArray.java
@@ -0,0 +1,25 @@
+public class subArray {
+  /**
+   * Return only a part of an array. For instance, given the array {1,2,3,4,5}, a
+   * sub array with start 1 and end 4
+   * would result in {2,3,4}, because the starting index is inclusive, but the
+   * ending index is exclusive.
+   *
+   * You will need to create a new array whose size is only the amound of elements
+   * requested between the start and
+   * end index, and then write a for loop which copies values from nums over to
+   * this new resulting array.
+   *
+   * @param nums  an array of ints.
+   * @param start the starting index, inclusive.
+   * @param end   the ending index, exclusive.
+   * @return a sub-array of nums containing the values between start and end.
+   */
+  public int[] sub(int[] nums, int start, int end) {
+    int[] newArr = new int[end - start];
+    for (int i = start; i < end; i++) {
+      newArr[i - start] = nums[i];
+    }
+    return newArr;
+  }
+}
